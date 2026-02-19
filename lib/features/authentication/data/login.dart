cac669eea8e0efdfc8774149432dbfee3e5cbdd6
danitunka <danieltunka42@gmail.com>
2026-02-19 22:47:45 +0300

    Login page updated

---
+++ lib/features/authentication/data/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Form(
+        child:Padding(
+          padding: EdgeInsets.all(20),
+          child:Column(
+            children: [
+              SizedBox(height:30),
+              Text(
+                'Welcome back to your rewards', style:TextStyle(fontSize:24,color:Colors.white60)
+              )
+            ],)
+          ),)
+    );
+  }
+}
