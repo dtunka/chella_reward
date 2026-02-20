aada2cea514bce5ac5ba988a9270f29cf27a901d
danitunka <danieltunka23gmail.com>
2026-02-20 17:57:49 +0300

    Login page created

--- lib/features/authentication/presentation/login.dart
+++ lib/features/authentication/presentation/login.dart
@@ -16,6 +16,8 @@ class _LoginPage extends State<LoginPage> {
           child:Column(
             children: [
               SizedBox(height:30),
+              Text('Chella', style: TextStyle(fontSize:26,color:Colors.yellow)),
+              SizedBox(height:10),
               Text(
                 'Welcome back to your rewards', style:TextStyle(fontSize:24,color:Colors.white60)
               )
