46ee7faf08cac0664e2d4e7bc25ce26ea842827a
danitunka <danieltunka23gmail.com>
2026-02-21 20:52:36 +0300

    login page modified

--- lib/features/authentication/presentation/login.dart
+++ lib/features/authentication/presentation/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -7,22 +8,209 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
+  bool _isPasswordVisible = false;
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  void _login() async {
+    if (_formKey.currentState!.validate()) {
+      String username = _usernameController.text.trim();
+      String password = _passwordController.text.trim();
+      final authProvider = Provider.of<AuthProvider>(context, listen: false);
+      await authProvider.login(username, password);
+      if (authProvider.currentUser != null) {
+        final snackBar = ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(authProvider.currentUser!.welcomeUser()),
+            duration: Duration(seconds: 1),
+          ),
+        );
+        // waits snackbar to be closed before navigating to /home
+        await snackBar.closed;
+        Navigator.pushNamed(context, '/home');
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(authProvider.error!)));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Form(
-        child:Padding(
+      body: Form(
+        key: _formKey,
+        child: Padding(
           padding: EdgeInsets.all(20),
-          child:Column(
+          child: Column(
             children: [
-              SizedBox(height:30),
-              Text('Chella', style: TextStyle(fontSize:26,color:Colors.yellow)),
-              SizedBox(height:10),
+              SizedBox(height: 30),
               Text(
-                'Welcome back to your rewards', style:TextStyle(fontSize:24,color:Colors.white60)
-              )
-            ],)
-          ),)
+                'Chella',
+                style: TextStyle(fontSize: 26, color: Colors.yellow),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Welcome back to your rewards',
+                style: TextStyle(fontSize: 24, color: Colors.white60),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Login to Account",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'username',
+                      style: TextStyle(fontSize: 16, color: Colors.white60),
+                    ),
+                    SizedBox(height: 5),
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        hintText: 'Enter your name',
+                        filled: true,
+                        fillColor: Colors.white10,
+                        hintStyle: TextStyle(color: Colors.white60),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'password',
+                      style: TextStyle(fontSize: 16, color: Colors.white60),
+                    ),
+                    SizedBox(height: 5),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: !_isPasswordVisible,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.white30,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (_isPasswordVisible) {
+                                _isPasswordVisible = false;
+                              } else {
+                                _isPasswordVisible = true;
+                              }
+                            });
+                          },
+                        ),
+                        hintText: 'Enter your password',
+                        hintStyle: TextStyle(
+                          color: Colors.white30,
+                          fontSize: 14,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white10,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        'Forgot password?',
+                        style: TextStyle(
+                          color: Colors.yellowAccent[700],
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _login,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Login to Chella",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Don't have an account",
+                        style: TextStyle(color: Colors.white38),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                     SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => RegisterPage(),
+                                ),
+                              );
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black12,
+                            side: BorderSide(color: Colors.yellow),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            "Create New Account",
+                            style: TextStyle(
+                              color: Colors.yellow[600],
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+               SizedBox(height: 5),
+                Text(
+                  'By logging in, you agree to our Terms and Conditions',
+                  style: TextStyle(fontSize: 10, color: Colors.white30),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
