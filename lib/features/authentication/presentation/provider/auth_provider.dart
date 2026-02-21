f3bd008b1a2c442d82e56a6702cbd66de8c7873c
danitunka <danieltunka23gmail.com>
2026-02-21 21:54:20 +0300

    Authentication provider file is created

---
+++ lib/features/authentication/presentation/provider/auth_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier {
+  // Initialize repo from the data layer
+  final AuthRepository _repository;
+  //to track loading in our app's state.
+  UserEntity? _currentUser;
+  bool _isLoading = false;
+  bool _userRegistered = false;
+  String? _error;
+  AuthProvider(this._repository);
+  //! THE FOLLOWING ARE GETTER METHODS TO GET 
+  // THIS VARIABLES IN OTHER CLASSES !
+  bool get isLoading => _isLoading;
+  bool get userRegistered => _userRegistered;
+  String? get error => _error;
+  UserEntity? get currentUser => _currentUser;
+  Future<void> login(String username, String password) async {
+    _isLoading = true;
+    _currentUser = null;
+    notifyListeners();
+    try {
+      final UserModel user = await _repository.login(username, password);
+      _currentUser = UserEntity(
+        id: user.id,
+        username: user.username,
+        fullName: user.fullName,
+        referralCode: user.referralCode,
+        referredBy: user.referredBy,
+        amount: user.amount,
+        totalEarned: user.totalEarned,
+        totalReferred: user.totalReferred,
+      );
+      _error = null;
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
